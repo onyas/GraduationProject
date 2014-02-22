ba027ae9c7e506d91a64309eacf7ae64db598133
onyas <zheng0518@163.com>
2014-02-22 15:57:29 +0800

    对话框中确定和取消的点击事件

--- PhoneGuard/src/com/onyas/phoneguard/ui/PhoneProtectedActivity.java
+++ PhoneGuard/src/com/onyas/phoneguard/ui/PhoneProtectedActivity.java
@@ -1,73 +1,116 @@
 package com.onyas.phoneguard.ui;
 
-import com.onyas.phoneguard.R;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class PhoneProtectedActivity extends Activity {
+import com.onyas.phoneguard.R;
+
+public class PhoneProtectedActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = "PhoneProtectedActivity";
+	private EditText et_password,et_password_confirm;
 	private SharedPreferences sp;
 	private Dialog dialog;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		if(isAlreadySavePwd())
-		{
+		if (isAlreadySavePwd()) {
 			Log.i(TAG, "存入密码，正常登录");
 			showNormalDialog();
-		}else
-		{
+		} else {
 			Log.i(TAG, "第一次登陆，需要设置密码");
 			showFirstDialog();
 		}
-		
-		
+
 	}
 
-
 	/**
-	 *显示第一次设置密码的对话框
+	 * 显示第一次设置密码的对话框
 	 */
 	private void showFirstDialog() {
 		dialog = new Dialog(this, R.style.MyDialog);
-		dialog.setContentView(R.layout.first_mydialog);
+		// dialog.setContentView(R.layout.first_mydialog);
+		View view = View.inflate(this, R.layout.first_mydialog, null);
+		 et_password = (EditText) view
+				.findViewById(R.id.et_first_entry_pwd);
+		et_password_confirm = (EditText) view
+				.findViewById(R.id.et_first_entry__confrim_pwd);
+		Button bt_ok = (Button) view.findViewById(R.id.bt_first_entry_confirm);
+		Button bt_cancel = (Button) view
+				.findViewById(R.id.bt_first_entry_cancel);
+		bt_ok.setOnClickListener(this);
+		bt_cancel.setOnClickListener(this);
+		dialog.setContentView(view);
+		dialog.setCancelable(false);
 		dialog.show();
 	}
-
 
 	/**
 	 * 显示要登陆的对话框
 	 */
 	private void showNormalDialog() {
-		
-	}
 
+	}
 
 	/**
 	 * 判断SharePreferences中是否已经存入密码
+	 * 
 	 * @return
 	 */
 	private boolean isAlreadySavePwd() {
 		sp = getSharedPreferences("config", Context.MODE_PRIVATE);
-		String pwd =sp.getString("password", null);
-		if(pwd==null)
-		{
+		String pwd = sp.getString("password", null);
+		if (pwd == null) {
 			return false;
-		}else
-		{
+		} else {
 			return true;
 		}
 	}
-	
-	
-	
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.bt_first_entry_confirm://点击确定时
+			String password = et_password.getText().toString().trim();
+			String pwd_confirm = et_password_confirm.getText().toString().trim();
+			if(TextUtils.isEmpty(password) || TextUtils.isEmpty(pwd_confirm))
+			{
+				Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+				return ;
+			}else
+			{
+				if(pwd_confirm.equals(password))
+				{
+					Editor editor = sp.edit();
+					editor.putString("password", password);
+					editor.commit();
+					Toast.makeText(this, "密码设置成功", Toast.LENGTH_SHORT).show();
+					dialog.dismiss();
+				}else
+				{
+					Toast.makeText(this, "两次密码不同", Toast.LENGTH_SHORT).show();
+					return ;
+				}
+			}
+			break;
+		case R.id.bt_first_entry_cancel://点击取消时
+			dialog.dismiss();//自定义的对话框，要显示的关闭
+			break;
+		}
+	}
+
 }
