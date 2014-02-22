f08de7c434f2a17467793c8d2dc8c14f74a0189f
onyas <zheng0518@163.com>
2014-02-22 15:18:11 +0800

    自定义对话框(第一次设置密码)

--- PhoneGuard/src/com/onyas/phoneguard/ui/PhoneProtectedActivity.java
+++ PhoneGuard/src/com/onyas/phoneguard/ui/PhoneProtectedActivity.java
@@ -1,7 +1,73 @@
 package com.onyas.phoneguard.ui;
 
+import com.onyas.phoneguard.R;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
 
 public class PhoneProtectedActivity extends Activity {
 
+	private static final String TAG = "PhoneProtectedActivity";
+	private SharedPreferences sp;
+	private Dialog dialog;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if(isAlreadySavePwd())
+		{
+			Log.i(TAG, "存入密码，正常登录");
+			showNormalDialog();
+		}else
+		{
+			Log.i(TAG, "第一次登陆，需要设置密码");
+			showFirstDialog();
+		}
+		
+		
+	}
+
+
+	/**
+	 *显示第一次设置密码的对话框
+	 */
+	private void showFirstDialog() {
+		dialog = new Dialog(this, R.style.MyDialog);
+		dialog.setContentView(R.layout.first_mydialog);
+		dialog.show();
+	}
+
+
+	/**
+	 * 显示要登陆的对话框
+	 */
+	private void showNormalDialog() {
+		
+	}
+
+
+	/**
+	 * 判断SharePreferences中是否已经存入密码
+	 * @return
+	 */
+	private boolean isAlreadySavePwd() {
+		sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+		String pwd =sp.getString("password", null);
+		if(pwd==null)
+		{
+			return false;
+		}else
+		{
+			return true;
+		}
+	}
+	
+	
+	
 }
