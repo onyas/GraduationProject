395d9a840570d04b2bef7b2427a0764cbe92bac8
onyas <zheng0518@163.com>
2014-02-26 22:45:16 +0800

    在android服务中监听电话的状态，来电时在手机屏幕上显示来电所在地

---
+++ PhoneGuard/src/com/onyas/phoneguard/service/PhoneNumberService.java
@@ -0,0 +1,101 @@
+package com.onyas.phoneguard.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.TextView;
+
+import com.onyas.phoneguard.engine.PhoneAddressEngine;
+
+public class PhoneNumberService extends Service {
+
+	private static final String TAG = "PhoneNumberService";
+	private MyPhoneStateListener listener;
+	private TelephonyManager tm;
+	private WindowManager wm;
+	private TextView tv;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		listener = new MyPhoneStateListener();
+
+		tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+
+	}
+
+	private class MyPhoneStateListener extends PhoneStateListener {
+
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			super.onCallStateChanged(state, incomingNumber);
+
+			switch (state) {
+			case TelephonyManager.CALL_STATE_IDLE:// 手机静止状态，没有呼叫
+				if (tv != null) {
+					wm.removeView(tv);
+					tv = null;
+				}
+				break;
+			case TelephonyManager.CALL_STATE_RINGING:// 响铃状态
+				Log.i(TAG, "incoming num" + incomingNumber);
+				String address = PhoneAddressEngine.getAddress(incomingNumber);
+				Log.i(TAG, address);
+				// Toast.makeText(getApplicationContext(), address,
+				// Toast.LENGTH_SHORT).show();
+				showLocation(address);
+				break;
+			case TelephonyManager.CALL_STATE_OFFHOOK:// 接通电话
+				if (tv != null) {
+					wm.removeView(tv);
+					tv = null;
+				}
+				break;
+			}
+
+		}
+
+	}
+
+	/**
+	 * 在手机屏幕上显示手机号码归属地
+	 * 
+	 * @param address
+	 */
+	private void showLocation(String address) {
+		WindowManager.LayoutParams params = new LayoutParams();
+		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+				| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+				| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+		params.format = PixelFormat.TRANSLUCENT;
+		params.type = WindowManager.LayoutParams.TYPE_TOAST;
+
+		tv = new TextView(getApplicationContext());
+		tv.setText(address);
+		
+		wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+		wm.addView(tv, params);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		tm.listen(listener, PhoneStateListener.LISTEN_NONE);
+		listener = null;
+	}
+}
--- PhoneGuard/src/com/onyas/phoneguard/ui/Fun8AtoolsActivity.java
+++ PhoneGuard/src/com/onyas/phoneguard/ui/Fun8AtoolsActivity.java
@@ -2,16 +2,24 @@ package com.onyas.phoneguard.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.onyas.phoneguard.R;
+import com.onyas.phoneguard.service.PhoneNumberService;
 
 public class Fun8AtoolsActivity extends Activity implements OnClickListener {
 
 	private TextView tv_atool_query;
+	private TextView tv_atool_serstate;
+	private CheckBox cb_atool_change;
+	private Intent serviceIntent;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,27 @@ public class Fun8AtoolsActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.fun_8atools);
 		
 		tv_atool_query = (TextView) findViewById(R.id.tv_atool_query);
+		tv_atool_serstate = (TextView) findViewById(R.id.tv_atool_servicestate);
+		cb_atool_change = (CheckBox) findViewById(R.id.cb_atool_servicechange);
+		
+		serviceIntent = new Intent(this, PhoneNumberService.class);
+		
+		cb_atool_change.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked){
+					startService(serviceIntent);
+					tv_atool_serstate.setTextColor(getResources().getColor(R.color.textColor));
+					tv_atool_serstate.setText("归属地服务已开启");
+				}else{
+					stopService(serviceIntent);
+					tv_atool_serstate.setTextColor(Color.RED);
+					tv_atool_serstate.setText("归属地服务未开启");
+				}
+			}
+		});
+		
 		
 		tv_atool_query.setOnClickListener(this);
 		
