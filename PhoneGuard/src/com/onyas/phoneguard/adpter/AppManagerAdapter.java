5a93b3d9fb5ba3305fe427a95f01521401071bcf
onyas <zheng0518@163.com>
2014-03-03 20:37:54 +0800

    ListView的优化，复用历史view对象，不用每次都重新加载

--- PhoneGuard/src/com/onyas/phoneguard/adpter/AppManagerAdapter.java
+++ PhoneGuard/src/com/onyas/phoneguard/adpter/AppManagerAdapter.java
@@ -3,6 +3,7 @@ package com.onyas.phoneguard.adpter;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,10 +15,13 @@ import com.onyas.phoneguard.domain.AppInfo;
 
 public class AppManagerAdapter extends BaseAdapter {
 
+	private static final String TAG = "AppManagerAdapter";
 	private List<AppInfo> infos;
 	private Context context;
-	
-	public AppManagerAdapter(List<AppInfo> infos,Context context) {
+	private static ImageView iv;
+	private static TextView tv;
+
+	public AppManagerAdapter(List<AppInfo> infos, Context context) {
 		super();
 		this.infos = infos;
 		this.context = context;
@@ -38,13 +42,24 @@ public class AppManagerAdapter extends BaseAdapter {
 		return position;
 	}
 
+	/**
+	 * 每个条目在显示的时候都会调用这个方法 convertView 转化view对象，历史view对象的缓存,就是拖动时候被回收掉的view对象
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
+		View view;
 		AppInfo appinfo = infos.get(position);
-		View view = View.inflate(context, R.layout.fun_3app_item, null);
-		ImageView iv = (ImageView) view.findViewById(R.id.iv_appmanager_function);
-		TextView tv = (TextView) view.findViewById(R.id.tv_appmanager_name);
+		if (convertView == null) {
+			// 通过资源文件，创建view对象
+			Log.i(TAG, "通过资源文件，创建view对象");
+			view = View.inflate(context, R.layout.fun_3app_item, null);
+		} else {
+			// 使用历史缓存view对象，不用反复调用inflate方法
+			Log.i(TAG, "使用历史缓存view对象");
+			view = convertView;
+		}
+		iv = (ImageView) view.findViewById(R.id.iv_appmanager_function);
+		tv = (TextView) view.findViewById(R.id.tv_appmanager_name);
 		iv.setImageDrawable(appinfo.getIcon());
 		tv.setText(appinfo.getAppname());
 		return view;
