d33eda17b433e4c25780bcde4c1b79be8db9f36b
onyas <zheng0518@163.com>
2014-03-08 11:03:07 +0800

    保存用户的操作到sp,然后根据sp里面的内容，初始化checkbox的状态，还有是否开始服务

--- PhoneGuard/src/com/onyas/phoneguard/ui/Fun9SettingCenterActivity.java
+++ PhoneGuard/src/com/onyas/phoneguard/ui/Fun9SettingCenterActivity.java
@@ -1,7 +1,10 @@
 package com.onyas.phoneguard.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -16,29 +19,45 @@ public class Fun9SettingCenterActivity extends Activity {
 	private TextView tv_content;
 	private CheckBox cb_status;
 	private Intent wdservice;
+	private SharedPreferences sp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fun_9setting);
+
+		sp=getSharedPreferences("config", Context.MODE_PRIVATE);
+		boolean lockappServiceStatus = sp.getBoolean("lockappServiceStatus", false);
+		
 		
 		tv_content = (TextView) findViewById(R.id.tv_setting_servicestate);
 		cb_status = (CheckBox) findViewById(R.id.cb_setting_servicechange);
 		wdservice = new Intent(this,WatchDogService.class);
 		
-		if(cb_status.isChecked()){
+		
+		//根据sp里面保存的状态，初始化复选框的状态
+		if(lockappServiceStatus){
+			cb_status.setChecked(true);
 			tv_content.setText("程序锁服务已开启");
 		}else{
+			cb_status.setChecked(false);
 			tv_content.setText("程序锁服务末开启");
 		}
 		
+		//根据复选状的选中状态，开启或停止服务，并且保存用户的操作到sp
 		cb_status.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				Editor editor = sp.edit();
 				if(isChecked){
 					startService(wdservice);
+					editor.putBoolean("lockappServiceStatus", true);
+					tv_content.setText("程序锁服务已开启");
 				}else{
 					stopService(wdservice);
+					editor.putBoolean("lockappServiceStatus", false);
+					tv_content.setText("程序锁服务末开启");
 				}
+				editor.commit();
 			}
 		});
 	}
