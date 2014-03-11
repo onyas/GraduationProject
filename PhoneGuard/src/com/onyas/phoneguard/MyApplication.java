98b73aecbeceb416c34229f9e422c74a4eb97cd2
onyas <zheng0518@163.com>
2014-03-11 20:54:13 +0800

    通过Application(应用程序上下文)来传递数据

---
+++ PhoneGuard/src/com/onyas/phoneguard/MyApplication.java
@@ -0,0 +1,11 @@
+package com.onyas.phoneguard;
+
+import android.app.Application;
+
+import com.onyas.phoneguard.domain.TaskInfo;
+
+public class MyApplication extends Application {
+
+	public TaskInfo taskinfo;
+	
+}
