5d9f6ff21570e4508a16845841c58c96018aeab2
onyas <zheng0518@163.com>
2014-03-12 20:10:56 +0800

    Widget的显示

---
+++ PhoneGuard/src/com/onyas/phoneguard/receiver/ProcessManagerWidget.java
@@ -0,0 +1,20 @@
+package com.onyas.phoneguard.receiver;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class ProcessManagerWidget extends AppWidgetProvider {
+
+	@Override
+	public void onDisabled(Context context) {
+		super.onDisabled(context);
+		
+	}
+
+	@Override
+	public void onEnabled(Context context) {
+		super.onEnabled(context);
+		
+	}
+
+}
