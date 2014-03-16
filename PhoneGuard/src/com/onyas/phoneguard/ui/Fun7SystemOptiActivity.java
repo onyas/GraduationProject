9c7d01e0387ff0cca2afd54136a3802fdb00c526
onyas <zheng0518@163.com>
2014-03-16 10:10:19 +0800

    清理SD卡上的缓存

--- PhoneGuard/src/com/onyas/phoneguard/ui/Fun7SystemOptiActivity.java
+++ PhoneGuard/src/com/onyas/phoneguard/ui/Fun7SystemOptiActivity.java
@@ -1,5 +1,7 @@
 package com.onyas.phoneguard.ui;
 
+import java.io.File;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,8 +16,13 @@ import android.content.pm.IPackageStatsObserver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.os.RemoteException;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,10 +30,12 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.onyas.phoneguard.R;
 import com.onyas.phoneguard.domain.CacheInfo;
+import com.onyas.phoneguard.util.FileUtil;
 import com.onyas.phoneguard.util.TextFormatter;
 
 public class Fun7SystemOptiActivity extends ListActivity {
@@ -36,14 +45,43 @@ public class Fun7SystemOptiActivity extends ListActivity {
 	private Map<String, CacheInfo> cacheMaps;
 	private PackageManager pm;
 	private List<PackageInfo> packinfos;
+	private TextView tv_status;
+	private SeekBar seekbar;
+	private SQLiteDatabase db;
+	private String path = "/data/data/com.onyas.phoneguard/files/clearpath.db";
+
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			String text = (String) msg.obj;
+			tv_status.setText(text);
+		};
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fun_7systemopti);
 		lv = getListView();
+		tv_status = (TextView) findViewById(R.id.tv_system_opti_status);
+		seekbar = (SeekBar) findViewById(R.id.seekbar);
+
 		cacheMaps = new HashMap<String, CacheInfo>();
 		pm = getPackageManager();
+
+		// 如果当前手机中没有数据库文件，则到类加载路径中去下载
+		File file = new File(path);
+		if (!file.exists()) {
+			try {
+				InputStream is = getClass().getClassLoader()
+						.getResourceAsStream("clearpath.db");
+				FileUtil.copyFile(is, this, "clearpath.db", MODE_PRIVATE);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		db = SQLiteDatabase.openDatabase(path, null,
+				SQLiteDatabase.OPEN_READONLY);
 
 		packinfos = pm
 				.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
@@ -60,31 +98,28 @@ public class Fun7SystemOptiActivity extends ListActivity {
 
 		adapter = new SystemOptiAdapter();
 		lv.setAdapter(adapter);
-		
-		
+
 		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				
+
 				/*
-				 *  <intent-filter>
-	                <action android:name="android.settings.APPLICATION_DETAILS_SETTINGS" />
-	                <category android:name="android.intent.category.DEFAULT" />
-	                <data android:scheme="package" />
-	            	</intent-filter>
-				 * */
+				 * <intent-filter> <action
+				 * android:name="android.settings.APPLICATION_DETAILS_SETTINGS"
+				 * /> <category android:name="android.intent.category.DEFAULT"
+				 * /> <data android:scheme="package" /> </intent-filter>
+				 */
 				CacheInfo cinfo = (CacheInfo) lv.getItemAtPosition(arg2);
 				Intent intent = new Intent();
 				intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
 				intent.addCategory("android.intent.category.DEFAULT");
-				intent.setData(Uri.parse("package:"+cinfo.getPackname()));
+				intent.setData(Uri.parse("package:" + cinfo.getPackname()));
 				startActivity(intent);
 			}
 		});
-		
-		
+
 	}
 
 	/**
@@ -179,6 +214,45 @@ public class Fun7SystemOptiActivity extends ListActivity {
 			return view;
 		}
 
+	}
+
+	/**
+	 * 按钮的点击事件，到数据库中去查当前手机的安装包中有没有指定的路径
+	 * 
+	 * @param view
+	 */
+	public void clear(View view) {
+		new Thread() {
+			public void run() {
+				List<PackageInfo> packinfos = getPackageManager()
+						.getInstalledPackages(
+								PackageManager.GET_UNINSTALLED_PACKAGES);
+				seekbar.setMax(packinfos.size());
+				int total = 0;
+				for (PackageInfo info : packinfos) {
+					String packname = info.packageName;
+					Cursor cursor = db.rawQuery(
+							"select filepath from softdetail where apkname=?",
+							new String[] { packname });
+					if (cursor.moveToNext()) {
+						String path = cursor.getString(0);
+						System.out.println("清除了" + packname);
+						File file = new File(
+								Environment.getExternalStorageDirectory(), path);
+						FileUtil.deleteDir(file);
+					}
+					total++;
+					seekbar.setProgress(total);
+					cursor.close();
+					Message msg = Message.obtain();
+					msg.obj = "清除" + packname;
+					handler.sendMessage(msg);
+				}
+				Message msg = Message.obtain();
+				msg.obj = "清除完毕";
+				handler.sendMessage(msg);
+			};
+		}.start();
 	}
 
 }
--- PhoneGuard/src/com/onyas/phoneguard/util/FileUtil.java
+++ PhoneGuard/src/com/onyas/phoneguard/util/FileUtil.java
@@ -1,15 +1,15 @@
 package com.onyas.phoneguard.util;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
+
+import android.content.Context;
 
 public class FileUtil {
 
 	/**
-	 * 从输入流复制文件
+	 * 把文件从输入流中复制到指定路径
 	 * 
 	 * @param is
 	 *            输入流
@@ -26,5 +26,48 @@ public class FileUtil {
 		}
 		fos.flush();
 		fos.close();
+	}
+
+	/**
+	 * 
+	 * 将输入流中的内容保存到应用程序自己的数据目录中
+	 * 
+	 * @param is
+	 *            输入流
+	 * @param context
+	 *            上下文
+	 * @param name
+	 *            要保存的名字
+	 * @param mode
+	 *            权限
+	 * @throws Exception
+	 */
+	public static void copyFile(InputStream is, Context context, String name,
+			int mode) throws Exception {
+		FileOutputStream fos = context.openFileOutput(name, mode);
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = is.read(buffer)) != -1) {
+			fos.write(buffer, 0, len);
+		}
+		fos.flush();
+		fos.close();
+	}
+
+	/**
+	 * 删除指定文件夹下的所有文件，包括子文件夹
+	 * 
+	 * @param dir
+	 *            要删除的目录
+	 */
+	public static void deleteDir(File dir) {
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				deleteDir(file);
+			} else {
+				file.delete();
+			}
+		}
 	}
 }
